dca765854641b86cfd098c73897beab2f4a88d5d
Toby Griffiths <toby@cubicmushroom.co.uk>
2017-10-20 10:53:41 +0100

    Added basic HTML

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-Hello weeerld!
+<html>
+<head>
+    <title>Getting Started</title>
+    <script src="https://unpkg.com/lodash@4.16.6"></script>
+</head>
+<body>
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
